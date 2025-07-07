e197095edefeeee4a3feecb96333736e91ce9657
Batuhan Tekin <ibrahim.batuhan.tekin.0742@gmail.com>
2025-07-07 21:35:52 +0300

    timeout for slow loris attack

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  app.getHttpServer().setTimeout(5);
+  app.getHttpServer().headersTimeout = 5000;
+  app.getHttpServer().keepAliveTimeout = 5000;
 }
 bootstrap();
